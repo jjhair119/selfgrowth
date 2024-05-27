9efed5a7ecce35cb8d2b1e33aa45f85360b27e52
mjwoon <jwmoon@hanyang.ac.kr>
2024-05-27 15:28:37 +0900

    Delete src directory

--- src/Diary/components/TitleArea.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const TitleArea = styled.div`
-  width: 307px;
-  height: 51px;
-  margin: 0px 208px 498px 205px;
-`;
-
-export default TitleArea;
--- src/Profile.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-interface ProfileProps {}
-
-const Profile: React.FC<ProfileProps> = () => {
-  return <div>Profile</div>;
-};
-
-export default Profile;
