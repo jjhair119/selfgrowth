ba4129a69ddd4b06434ad47ffa44f9ea52130a37
Baetato <diana021015@gmail.com>
2024-06-02 22:49:57 +0900

    FIX:HOME 이미지 불러오기, Profile 이미지 불러오기

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,10 +1,26 @@
-//마이페이지
+// 마이페이지
 import styled from "styled-components";
 
 interface ProfileProps {}
 
+const ProfileContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; // 화면 전체 높이를 차지하게 합니다.
+`;
+
+const ProfileImage = styled.img`
+  max-width: 100%;
+  height: auto; // 이미지의 비율을 유지하면서 크기를 조절합니다.
+`;
+
 const Profile: React.FC<ProfileProps> = () => {
-  return <div>Profile</div>;
+  return (
+    <ProfileContainer>
+      <ProfileImage src="./myprofile.svg" alt="profile" />
+    </ProfileContainer>
+  );
 };
 
 export default Profile;
